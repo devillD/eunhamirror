cf868e76c4f69ed10cc8d18b56e58edc4d221d88
Devill Don <devilldon5@gmail.com>
2022-06-22 16:00:58 +0600

    Update aria.sh

--- aria.sh
+++ aria.sh
@@ -7,7 +7,7 @@ USERAGENT=$(echo "Mozilla/5.0 (Linux; Android 12; SM-A205U) AppleWebKit/537.36 (
 aria2c --enable-rpc=true --check-certificate=false --daemon=true \
    --max-connection-per-server=10 --rpc-max-request-size=1024M --quiet=true \
    --bt-stop-timeout=$TORRENT_TIMEOUT --min-split-size=10M --split=10 --allow-overwrite=true \
-   --max-overall-download-limit=0 --bt-tracker="[$TRACKERS]" --disk-cache=32M \
+   --max-overall-download-limit=0 --bt-tracker="[$TRACKERS]" --disk-cache=32M --rpc-listen-port=7389 \
    --max-overall-upload-limit=1K --max-concurrent-downloads=15 --summary-interval=0 \
    --peer-id-prefix=-qB4420- --user-agent="[$USERAGENT]" --peer-agent=qBittorrent/4.4.2 \
    --bt-enable-lpd=true --seed-time=0 --max-file-not-found=0 --max-tries=20 \
